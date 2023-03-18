ebd888358ee98922545df3f6f739daa4e1bd5650
nyakagwafred <nyakagwa.fred@gmail.com>
2023-03-18 23:54:50 +0300

    user model setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import colors from 'colors';
 
 dotenv.config();
-//const db = config.get('mongoURI');
 const db = process.env.MONGO_URI || 5000;
-
+// Connect to MongoDB
 const connectDB = async () => {
 	try {
 		await mongoose.connect(db);
 
-		console.log('MongoDB Connected...');
+		console.log('DB Connected'.underline.green);
 	} catch (err) {
-		console.log('DB Not connected');
+		console.log('DB Not connected'.red);
 		console.error(err.message);
 		// Exit process with failure
 		process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-
+import colors from 'colors';
+const app = express();
 dotenv.config();
 
 // Connect Database
 connectDB();
 
-const app = express();
+// Init Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.send('API is running');
 });
 
+// Serve static assets in production
 const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.inverse
+			.red,
+	),
 );
