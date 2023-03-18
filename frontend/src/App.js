916f7eea63a0747afc38370c073e2bd9ef9ddb22
nyakagwafred <nyakagwa.fred@gmail.com>
2023-03-18 04:16:04 +0300

    react setup complete

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Welcome to Nomadic Tours.</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
