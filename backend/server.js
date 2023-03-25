97d9838b7a11cd726b8778644b7d32ee55e9642b
nyakagwafred <nyakagwa.fred@gmail.com>
2023-03-26 02:17:35 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,21 @@ app.get('/', (req, res) => {
 });
 
 // Serve static assets in production
+const __dirname = path.resolve();
+//app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....');
+	});
+}
+//
 const PORT = process.env.PORT || 5000;
 
 app.listen(
